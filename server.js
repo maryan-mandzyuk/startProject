93d731ce8c2f7c6f069f5cf8df5b873c3c70eec6
mandzyuk-maryan <mandzyuk.maryan@gmail.com>
2019-12-25 17:55:36 +0200

    first backend version

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+connectDB();
+
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+app.use('/api/users', require('./src/routes/api/users'));
+app.use('/api/auth', require('./src/routes/api/auth'));
+app.use('/api/profile', require('./src/routes/api/profile'));
+app.use('/api/posts', require('./src/routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
